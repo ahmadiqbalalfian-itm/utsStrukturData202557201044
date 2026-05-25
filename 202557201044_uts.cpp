d166815de3638d3841b638f6c1ec8acd7690f2df
ahmadiqbalalfian-itm <ahmadiqbalalfian@gmail.com>
2026-05-25 10:16:41 +0700

    perbaiki method lembur, batasan jam, dan tabel output

--- 202557201044_uts.cpp
+++ 202557201044_uts.cpp
@@ -2,37 +2,55 @@
 #include <iomanip>
 using namespace std;
 
-	struct pegawai {
-		int waktu,honor;
-	};
+struct pegawai {
+	int waktu, honor;
+};
 
-float gaji(int lembur){
-	return lembur*10000;
+float lembur(int waktu){
+	return (waktu - 8) * 10000;
 }
 
 int main() {
-	cout<<"======================================\nPENENTUAN HONOR HARIAN PEGAWAI HONORER\n======================================";
+	cout << "======================================\n";
+	cout << "PENENTUAN HONOR HARIAN PEGAWAI HONORER\n";
+	cout << "======================================\n";
 	
 	int jumlah;
-	cout<<"\n\nBerapa pegawai yang akan didata\t:";
-	cin>>jumlah;
+	cout << "\nBerapa pegawai yang akan didata\t: ";
+	cin >> jumlah;
 	
-	pegawai iqbal [jumlah];
+	pegawai iqbal[jumlah];
 	
-	for (int f=0;f<jumlah;f++){
-		cout<<"\nBerapa jam pegawai ke "<<f+1<<" bekerja\t:";
-		cin>> iqbal[f].waktu;
+	for (int f = 0; f < jumlah; f++){
+		cout << "\nBerapa jam pegawai ke " << f+1 << " bekerja (minimal 8 maksimal 12)\t: ";
+		cin >> iqbal[f].waktu;
 		
-		iqbal[f].honor=80000+ gaji(iqbal[f].waktu-8);
-		cout<<"------------------------------------";
+		if (iqbal[f].waktu < 8 || iqbal[f].waktu > 12){
+			cout << "\n**********************************\n";
+			cout << "MOHON MASUKKAN WAKTU YANG SESUAI\n";
+			cout << "**********************************\n";
+			f--;
+		} else {
+			iqbal[f].honor = 80000 + lembur(iqbal[f].waktu);
+		}
 	}
 	
-	system ("cls");
+	system("cls");
 	
-	cout<<"======================================\nPENENTUAN HONOR HARIAN PEGAWAI HONORER\n======================================\n";
-	cout<<setiosflags(ios::left)<<setw(3)<<"No."<<setw(10)<<"Jam Kerja"<<setw(20)<<"Honor"<<"\n";
-		
-	for(int i=0;i<jumlah;i++){
-		cout<<setiosflags(ios::left)<<setw(3)<<i+1<<setw(10)<<iqbal[i].waktu<<setw(20)<<iqbal[i].honor<<"\n";
+	cout << "======================================\n";
+	cout << "PENENTUAN HONOR HARIAN PEGAWAI HONORER\n";
+	cout << "======================================\n";
+	cout << left << setw(5) << "No." 
+	     << setw(12) << "Jam Kerja" 
+	     << setw(20) << "HONOR TOTAL" << "\n";
+	
+	cout << "--------------------------------------\n"; 
+	
+	for(int i = 0; i < jumlah; i++){
+		cout << left << setw(5) << i + 1 
+		     << setw(12) << iqbal[i].waktu 
+		     << "Rp " << iqbal[i].honor << "\n";
 	}
+	
+	return 0;
 }
